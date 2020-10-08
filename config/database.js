8a32958c9c794ac8034c1fdbe3a63101bd7cfbf0
petermada <petermada@gmail.com>
2020-10-08 20:41:16 +0100

    Add production, dev database config

--- config/database.js
+++ config/database.js
@@ -1,22 +1,15 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-      },
-      options: {
-        ssl: false,
-      },
+    defaultConnection: 'default',
+    connections: {
+        default: {
+            connector: 'bookshelf',
+            settings: {
+                client: 'sqlite',
+                filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+            },
+            options: {
+                useNullAsDefault: true,
+            },
+        },
     },
-  },
 });
