38d70f2def484da6eed6007f0214f2f91142742b
petermada <petermada@gmail.com>
2020-10-08 21:08:44 +0100

    Add upload config

---
+++ extensions/upload/config.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+        provider: 'cloudinary',
+        providerOptions: {
+            cloud_name: env('dabxbptiu'),
+            api_key: env('866313241611623'),
+            api_secret: env('_UYV3gcmE3eR1EEJ_2glVpZEP5Y'),
+        },
+    },
+    // ...
+});
