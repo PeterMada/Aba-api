8fc6eec548919dfd71d7f77aaa13f3d4f96ebbb3
PeterMada <petermada@gmail.com>
2021-04-07 16:38:43 +0200

    Add config certificate

--- config/database.js
+++ config/database.js
@@ -6,6 +6,9 @@ module.exports = ({ env }) => ({
             settings: {
                 client: 'sqlite',
                 filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+                ssl: {
+                    rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+                },
             },
             options: {
                 useNullAsDefault: true,
