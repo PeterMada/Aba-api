509672fa2a587a878ccd2b1d831005f63c1c6ef5
Peter <petermada@gmail.com>
2021-09-29 14:32:34 +0200

    Enable emails

--- api/email/controllers/email.js
+++ api/email/controllers/email.js
@@ -6,47 +6,46 @@ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
  * to customize this controller
  */
 module.exports = {
-    create: async ctx => {
-        const nodemailer = require('nodemailer');
-        let entity;
+  create: async ctx => {
+    const nodemailer = require('nodemailer');
+    let entity;
 
-        if (ctx.is('multipart')) {
-            const { data, files } = parseMultipartData(ctx);
-            entity = await strapi.services.email.create(data, { files });
-        } else {
-            entity = await strapi.services.email.create(ctx.request.body);
-        }
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.email.create(data, { files });
+    } else {
+      entity = await strapi.services.email.create(ctx.request.body);
+    }
 
-        let transport = nodemailer.createTransport({
-            host: "smtp.seznam.cz",
-            port: 465,
-            auth: {
-                user: "madapeterr",
-                pass: "seznamMpm4Na1h"
-            }
-        });
+    let transport = nodemailer.createTransport({
+      host: "smtp.seznam.cz",
+      port: 465,
+      auth: {
+        user: "madapeterr",
+        pass: "seznamMpm4Na1h"
+      }
+    });
 
-        const message = {
-            from: 'ABABrno.cz <madapeterr@seznam.cz>',
-            // to: `${ctx.request.body.SendEmailTo}, madapeterr@seznam.cz`,
-            to: `madapeterr@gmail.com, madapeterr@seznam.cz`,
-            replyTo: ctx.request.body.Email,
-            subject: 'Nová zpráva z ABABrno.cz',
-            text: `Zpráva z ABABrno.cz. 
+    const message = {
+      from: 'ABABrno.cz <madapeterr@seznam.cz>',
+      to: `${ctx.request.body.SendEmailTo}`,
+      replyTo: ctx.request.body.Email,
+      subject: 'Nová zpráva z ABABrno.cz',
+      text: `Zpráva z ABABrno.cz. 
                    Od: ${ctx.request.body.Jmeno}. 
                    Email: ${ctx.request.body.Email}
                    Zájem o: ${ctx.request.body.Interest}. 
                    Text zprávy: ${ctx.request.body.Text}`,
-            html: `Zpráva z ABABrno.cz. <br /> 
+      html: `Zpráva z ABABrno.cz. <br /> 
                    Od: <strong>${ctx.request.body.Jmeno}</strong>.<br /> 
                    Email: <strong>${ctx.request.body.Email}</strong>.<br />
                    Zájem o: <strong>${ctx.request.body.Interest}</strong>. <br /> 
                    Text zprávy: ${ctx.request.body.Text}`
-        };
-        transport.sendMail(message, function (err, info) { });
+    };
+    transport.sendMail(message, function (err, info) { });
 
-        return sanitizeEntity(entity, { model: strapi.models.email });
+    return sanitizeEntity(entity, { model: strapi.models.email });
 
-    },
+  },
 
 }
